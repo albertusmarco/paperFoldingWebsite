edaabea3fe45ed88e1272a7cfef3239ce29412fd
albertusmarco <albertusmarco@gmail.com>
2018-12-03 23:56:30 +0700

    asdad

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
+var teximgt;
 function setup() {
   // put setup code here
   createCanvas(500, 500, WEBGL);
+  // text = text('word');//text(str, x, y, [x2], [y2])
+  img = loadImage("asset/bobby.jpg");
 }
 
 function draw() {
@@ -14,7 +17,13 @@ function draw() {
   // ellipse(mouseX, mouseY, 80, 80);
   // rotateX(frameCount * 0.01);
   // rotateY(frameCount * 0.01);
-  background(0);//warna
+  background(0);//warna createCanvas
+  //warna box
+  // noStroke();
+  // fill(0,0,255);
   orbitControl(5,5);//control orbit()
+  push();
+  texture(img);
   box(100,150,100);//box([width], [Height], [depth], [detailX], [detailY])
+  pop();
 }
