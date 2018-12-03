ef77d30d2b41a2748156a352d689bfcd247ca942
albertusmarco <39873755+albertusmarco@users.noreply.github.com>
2018-12-03 13:53:09 +0700

    asadada

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  // put setup code here
+  createCanvas(500, 500, WEBGL);
+}
+
+function draw() {
+  // put drawing code here
+  // if(mouseIsPressed) {
+  //   fill(0);
+  // }
+  // else {
+  //   fill(255);
+  // }
+  // ellipse(mouseX, mouseY, 80, 80);
+  // rotateX(frameCount * 0.01);
+  // rotateY(frameCount * 0.01);
+  background(0);//warna
+  orbitControl(5,5);//control orbit()
+  box(100,100,100);//box([width], [Height], [depth], [detailX], [detailY])
+}
