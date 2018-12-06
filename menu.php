e8d4301d1926f0bc33ddd32e55f62e12b2bdaa04
dewisalmas <dewisalmasalsabila@gmail.com>
2018-12-06 15:46:54 +0700

    file php

---
+++ menu.php
@@ -0,0 +1,64 @@
+<?php
+include "koneksi.php";
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+ <!-- Site made with Mobirise Website Builder v4.8.4, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.8.4, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <!-- <link rel="shortcut icon" href="assets/images/logo2.png" type="image/x-icon"> -->
+  <meta name="description" content="">
+  <title>sendwish</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+</head>
+
+<body>
+<section class="menu cid-rbkgwRuNyn" once="menu" id="menu2-1">
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                
+                <span class="navbar-caption-wrap"><a class="navbar-caption text-white display-5" href="index.php">SENDWISH</a></span>
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true"><li class="nav-item">
+                    <a class="nav-link link text-black display-4" href="index.html#header10-3">
+                        Home</a>
+                </li><li class="nav-item"><a class="nav-link link text-black display-4" href="index.html#features15-9">
+                        Portfolio</a></li><li class="nav-item"><a class="nav-link link text-black display-4" href="index.html#testimonials-slider1-b">
+                        About</a></li><li class="nav-item"><a class="nav-link link text-black display-4" href="https://mobirise.co">
+                        Shop</a></li>
+                <li class="nav-item">
+                    <a class="nav-link link text-black display-4" href="https://mobirise.co">
+                        Sign In</a>
+                </li></ul>
+            
+        </div>
+    </nav>
+</section>
+
+  
+  
+</body>
+</html>
---
+++ papercup.php
@@ -0,0 +1,232 @@
+<?php
+include "menu.php";
+?>
+
+<!DOCTYPE html>
+<html >
+<head> 
+  
+  
+</head>
+<body>
+  
+<section class="engine"><a href="https://mobirise.info/d">website maker</a></section><section once="" class="cid-rblxeb4aPl" id="footer6-t">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            <div class="col-12">
+                <p class="mbr-text mb-0 mbr-fonts-style display-7">
+                    Find us on :</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="features12 cid-rblRmm98RR" id="features12-v">
+    
+    
+
+    
+
+    <div class="container">
+        <h2 class="mbr-section-title pb-2 mbr-fonts-style display-2">
+            Paper Cup</h2>
+        
+
+        <div class="media-container-row pt-5">
+            <div class="block-content align-right">
+                <div class="card pl-3 pr-3 pb-5">
+                    <div class="mbr-card-img-title">
+                        
+                        <div class="mbr-crt-title">
+                            <h4 class="card-title py-2 mbr-crt-title mbr-fonts-style display-7">
+                                Material : Single Wall</h4>
+                        </div>
+                    </div>                
+
+                    <div class="card-box">
+                        <p class="mbr-text mbr-section-text mbr-fonts-style display-7">
+                            Paper cup berdinding tunggal, bisa hot cup ataupun cold cup tergantung keberadaan lapisan PE-nya. Bentuknya tinggi menjulang, dimana diameter cup lebih kecil dibandingkan tinggi cup-nya. Cocok untuk minuman panas seperti kopi dan minuman dingin seperti juice buah
+                        </p>
+                    </div>
+                </div>
+
+                
+            </div>
+
+            <div class="mbr-figure" style="width: 26%;">
+                <img src="assets/images/paper-cup-300x300.png" alt="Mobirise" title="">
+            </div>
+
+            <div class="block-content align-left  ">
+                <div class="card pl-3 pr-3 pb-5">
+                    <div class="mbr-card-img-title">
+                        
+                        <div class="mbr-crt-title">
+                            <h4 class="card-title py-2 mbr-crt-title mbr-fonts-style display-7">
+                                Material : Flat wrap</h4>
+                        </div>
+                    </div>                
+
+                    <div class="card-box">
+                        <p class="mbr-text mbr-section-text mbr-fonts-style display-7">
+                            Paper cup dengan dinding ganda yaitu dinding pertama adalah paper cup-nya itu sendiri dan dinding kedua adalah kertas pelapis di bagian luarnya. Kertas pelapis ini dimaksudkan sebagai penahan panas agar konsumen lebih nyaman saat memegang cup berisi minuman panas. Cocok untuk menyajikan minuman panas tentunya
+                        </p>
+                    </div>
+                </div>
+
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="features12 cid-rbqKP746zY" id="features12-y">
+    
+    
+
+    
+
+    <div class="container">
+        <h2 class="mbr-section-title pb-2 mbr-fonts-style display-4"></h2>
+        
+
+        <div class="media-container-row pt-5">
+            <div class="block-content align-right">
+                <div class="card pl-3 pr-3 pb-5">
+                    <div class="mbr-card-img-title">
+                        
+                        <div class="mbr-crt-title">
+                            <h4 class="card-title py-2 mbr-crt-title mbr-fonts-style display-7">Material : Single wall dengan handle</h4>
+                        </div>
+                    </div>                
+
+                    <div class="card-box">
+                        <p class="mbr-text mbr-section-text mbr-fonts-style display-7">
+                            Paper cup berdinding tunggal dengan handle atau gagang di sisi luarnya. Gagang ini untuk memudahkan konsumen saat memegang cup yang berisi minuman panas. Cocok untuk minuman panas karena biasanya hanya memiliki lapisan PE di bagian dalam cup saja
+                        </p>
+                    </div>
+                </div>
+
+                <div class="card pl-3 pr-3 pb-5">
+                    <div class="mbr-card-img-title">
+                        
+                        <div class="mbr-crt-title">
+                            <h4 class="card-title py-2 mbr-crt-title mbr-fonts-style display-7">Material : Ice cream cup</h4>
+                        </div>
+                    </div>
+                    <div class="card-box">
+                        <p class="mbr-text mbr-section-text mbr-fonts-style display-7">
+                            Paper cup berdinding tunggal dengan lapisan PE di bagian dalam dan luar cup. Bentuknya pendek dan cenderung melebar karena diameter cup lebih besar daripada tingginya. Diameter yang besar dimaksudkan agar konsumen lebih mudah waktu menyendok isi cup. Sesuai namanya cup ini untuk makanan dingin yaitu es krim atau bisa juga untuk pudding
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mbr-figure" style="width: 41%;">
+                <img src="assets/images/paper-cup-300x300.png" alt="Mobirise" title="">
+            </div>
+
+            <div class="block-content align-left  ">
+                <div class="card pl-3 pr-3 pb-5">
+                    <div class="mbr-card-img-title">
+                        
+                        <div class="mbr-crt-title">
+                            <h4 class="card-title py-2 mbr-crt-title mbr-fonts-style display-7">Material : W barrier</h4>
+                        </div>
+                    </div>                
+
+                    <div class="card-box">
+                        <p class="mbr-text mbr-section-text mbr-fonts-style display-7">
+                            Paper cup berdinding ganda, yaitu dinding paper cup-nya sendiri dan dinding bagian luar berupa kertas pelapis penahan panas yang bertekstur gelombang. Dengan tekstur gelombang maka tingkat insulasi terhadap panas jauh lebih baik dibandingkan dengan flat wrap. Ada jarak antar puncak gelombang dengan dinding cup sehingga panas tidak akan langsung terasa di jari saat dipegang. Namun dari sisi branding flat wrap jauh lebih baik dibandingkan w barrier karena pada w barrier permukaannya yang bergelombang membuat merk atau grafis yang tercetak di atasnya tidak terlalu jelas terlihat
+                        </p>
+                    </div>
+                </div>
+
+                <div class="card pl-3 pr-3 pb-5">
+                    <div class="mbr-card-img-title">
+                        
+                        <div class="mbr-crt-title">
+                            <h4 class="card-title py-2 mbr-crt-title mbr-fonts-style display-7"></h4>
+                        </div>
+                    </div>
+                    <div class="card-box">
+                        <p class="mbr-text mbr-section-text mbr-fonts-style display-7"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mbr-section article content3 cid-rblS0kksEy" id="content3-w">
+      
+     
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="row col-12 col-md-8">
+                <div class="col-12 col-md-6 pr-lg-4 mbr-text mbr-fonts-style display-7"><p><strong>
+                     Material : 3 wall</strong></p><p>Paper cup berdinding tiga lapis. Lapis pertama adalah dinding cup-nya, di tengahnya ada lapisan karton corrugated (gelombang) dan di lapis terluar kertas karton yang membungkus seluruh permukaan cup. Bisa dibilang menyerupai gabungan flat wrap dan w barrier. Tingkat insulasi panasnya jelas paling optimal diantara model lainnya dan permukaan luarnya yang rata membuat merk atau cetakan grafis di atasnya dapat terlihat dengan jelas. Namun dengan adanya penambahan dua dinding penahan panas tersebut membuat harga paper cup model ini jadi yang paling mahal</p></div>
+                <div class="col-12 col-md-6 pl-lg-4 mbr-text mbr-fonts-style display-7"><p><strong>Material : Bowl</strong></p><p>Make your own website in a few clicks! Mobirise helps you cut down development time by providing you with a flexible website editor with a drag and drop interface. Mobirise Website Builder creates responsive, retina and mobile friendly websites in a few clicks. Mobirise is one of the easiest website development tools available today. It also gives you the freedom to develop as many websites as you like given the fact that it is a desktop app.
+                </p></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section once="" class="cid-rbkA1DU2DQ mbr-reveal" id="footer7-u">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            
+            <div class="row social-row">
+                <div class="social-list align-right pb-2">
+                    
+                    
+                    
+                    
+                    
+                    
+                <div class="soc-item">
+                        <a href="https://twitter.com/mobirise" target="_blank">
+                            <span class="mbr-iconfont mbr-iconfont-social socicon-instagram socicon"></span>
+                        </a>
+                    </div><div class="soc-item">
+                        <a href="https://twitter.com/mobirise" target="_blank">
+                            <span class="mbr-iconfont mbr-iconfont-social socicon-whatsapp socicon"></span>
+                        </a>
+                    </div><div class="soc-item">
+                        <a href="https://twitter.com/mobirise" target="_blank">
+                            <span class="mbr-iconfont mbr-iconfont-social socicon-facebook socicon"></span>
+                        </a>
+                    </div></div>
+            </div>
+            <div class="row row-copirayt">
+                <p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7"></p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/dropdown/js/script.min.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  
+  
+</body>
+</html>
