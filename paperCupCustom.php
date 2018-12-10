8c2f8c2147f57eb1f75191a49127c1cb1592ef38
albertusmarco <albertusmarco@gmail.com>
2018-12-11 02:39:13 +0700

    update paper cup

---
+++ paperCupCustom.php
@@ -0,0 +1,305 @@
+<!-- <div align="center">
+  <p>
+<div class="color" id="redscale">
+<div style='background-color:#f00;'>a</div>
+<div style='background-color:#e00;'>a</div>
+<div style='background-color:#d00;'>a</div>
+<div style='background-color:#c00;'>a</div>
+<div style='background-color:#b00;'>a</div>
+<div style='background-color:#a00;'>a</div>
+<div style='background-color:#900;'>a</div>
+<div style='background-color:#800;'>a</div>
+<div style='background-color:#700;'>a</div>
+<div style='background-color:#600;'>a</div>
+<div style='background-color:#500;'>a</div>
+<div style='background-color:#400;'>a</div>
+<div style='background-color:#300;'>a</div>
+<div style='background-color:#200;'>a</div>
+<div style='background-color:#100;'>a</div>
+<div style='background-color:#000;'>a</div>
+</div>
+<div class="color" id="greenscale">
+<div style='background-color:#0f0;'>a</div>
+<div style='background-color:#0e0;'>a</div>
+<div style='background-color:#0d0;'>a</div>
+<div style='background-color:#0c0;'>a</div>
+<div style='background-color:#0b0;'>a</div>
+<div style='background-color:#0a0;'>a</div>
+<div style='background-color:#090;'>a</div>
+<div style='background-color:#080;'>a</div>
+<div style='background-color:#070;'>a</div>
+<div style='background-color:#060;'>a</div>
+<div style='background-color:#050;'>a</div>
+<div style='background-color:#040;'>a</div>
+<div style='background-color:#030;'>a</div>
+<div style='background-color:#020;'>a</div>
+<div style='background-color:#010;'>a</div>
+<div style='background-color:#000;'>a</div>
+</div>
+<div class="color" id="bluescale">
+<div style='background-color:#00f;'>a</div>
+<div style='background-color:#00e;'>a</div>
+<div style='background-color:#00d;'>a</div>
+<div style='background-color:#00c;'>a</div>
+<div style='background-color:#00b;'>a</div>
+<div style='background-color:#00a;'>a</div>
+<div style='background-color:#009;'>a</div>
+<div style='background-color:#008;'>a</div>
+<div style='background-color:#007;'>a</div>
+<div style='background-color:#006;'>a</div>
+<div style='background-color:#005;'>a</div>
+<div style='background-color:#004;'>a</div>
+<div style='background-color:#003;'>a</div>
+<div style='background-color:#002;'>a</div>
+<div style='background-color:#001;'>a</div>
+<div style='background-color:#000;'>a</div>
+</div>
+<div class="color" id="yellowscale">
+<div style='background-color:#ff0;'>a</div>
+<div style='background-color:#ee0;'>a</div>
+<div style='background-color:#dd0;'>a</div>
+<div style='background-color:#cc0;'>a</div>
+<div style='background-color:#bb0;'>a</div>
+<div style='background-color:#aa0;'>a</div>
+<div style='background-color:#990;'>a</div>
+<div style='background-color:#880;'>a</div>
+<div style='background-color:#770;'>a</div>
+<div style='background-color:#660;'>a</div>
+<div style='background-color:#550;'>a</div>
+<div style='background-color:#440;'>a</div>
+<div style='background-color:#330;'>a</div>
+<div style='background-color:#220;'>a</div>
+<div style='background-color:#110;'>a</div>
+<div style='background-color:#000;'>a</div>
+</div>
+<div class="color" id="cyanscale">
+<div style='background-color:#0ff;'>a</div>
+<div style='background-color:#0ee;'>a</div>
+<div style='background-color:#0dd;'>a</div>
+<div style='background-color:#0cc;'>a</div>
+<div style='background-color:#0bb;'>a</div>
+<div style='background-color:#0aa;'>a</div>
+<div style='background-color:#099;'>a</div>
+<div style='background-color:#088;'>a</div>
+<div style='background-color:#077;'>a</div>
+<div style='background-color:#066;'>a</div>
+<div style='background-color:#055;'>a</div>
+<div style='background-color:#044;'>a</div>
+<div style='background-color:#033;'>a</div>
+<div style='background-color:#022;'>a</div>
+<div style='background-color:#011;'>a</div>
+<div style='background-color:#000;'>a</div>
+</div>
+</p>
+</div> -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Sendwish</title>
+
+    <style>
+    body
+    {
+      margin:0;
+    }
+    canvas
+    {
+      /* border: 1px solid black; */
+      /* width:1000px;
+      height:1000px; */
+    }
+    /* canvas {
+    padding: 10;
+    margin: auto;
+    display: block;
+    width: 800px;
+    height: 600px;
+    position: absolute;
+    top: 10;
+    bottom: 10;
+    left: 10;
+    right: 10;
+    } */
+    </style>
+
+    <!-- mobirise template -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="generator" content="Mobirise v4.8.4, mobirise.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+    <link rel="shortcut icon" href="assets/images/logo2.png" type="image/x-icon">
+    <meta name="description" content="">
+    <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+    <link rel="stylesheet" href="assets/tether/tether.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="assets/dropdown/css/style.css">
+    <link rel="stylesheet" href="assets/socicon/css/styles.css">
+    <link rel="stylesheet" href="assets/theme/css/style.css">
+    <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+
+    <!-- p5.js library -->
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.2/p5.js"></script>
+    <script src="js/sketch.js"></script> -->
+
+
+    <link rel="stylesheet" type="text/css" href="css/sizeSlider.css">
+    <link rel="stylesheet" type="text/css" href="css/color.css">
+  </head>
+  <body>
+    <section class="menu cid-rbkgwRuNyn" once="menu" id="menu2-1">
+        <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <div class="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </button>
+            <div class="menu-logo">
+                <div class="navbar-brand">
+
+                    <span class="navbar-caption-wrap"><a class="navbar-caption text-white display-5">SENDWISH : PAPERCUP CUSTOMIZE</a></span>
+                </div>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true"><li class="nav-item">
+                        <a class="nav-link link text-black display-4" href="index.php#header10-3">
+                            Home</a>
+                    </li><li class="nav-item"><a class="nav-link link text-black display-4" href="index.php#features15-9">
+                            Portfolio</a></li><li class="nav-item"><a class="nav-link link text-black display-4" href="index.php#testimonials-slider1-b">
+                            About</a></li><li class="nav-item"><a class="nav-link link text-black display-4" href="noshop.php">
+                            Shop</a></li>
+                    <li class="nav-item">
+                        <a class="nav-link link text-black display-4" href="https://mobirise.co">
+                            Sign In</a>
+                    </li></ul>
+
+            </div>
+        </nav>
+    </section>
+
+    <section style="background-color:#F5F5F5;">
+      <br/><br/><br/><br/>
+        <span style="float:left;margin-left:80px;margin-top:35px;background-color:#FFFFFF;">
+          <br/>
+          <a class="navbar-caption text-black display-5" style="margin:0.8cm;">SIZE</a>
+          <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <a id="size1">8 oz &nbsp;&nbsp;: 6 cm x 5 cm x 5 cm &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <a id="size2">12 oz : 8,5 cm x 5,8 cm x 10,5 cm &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <a id="size3">16 oz : 8,5 cm x 5,8 cm x 12,5 cm &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+            <a class="navbar-caption text-black display-5" style="margin:0.8cm;">COLOR</a>
+            <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;test
+        </span>
+        <!-- <br/> -->
+
+          <span id = "custom" style="float:center;margin-left:10px;">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <a class="navbar-caption text-black display-5">CHOOSE SIDE :</a>
+            &nbsp;
+            <select id="side" style="width:125px">
+              <option value="front">FRONT</option>
+              <option value="back">BACK</option>
+              <option value="left">LEFT</option>
+              <option value="right">RIGHT</option>
+              <option value="top">TOP</option>
+              <option value="down">DOWN</option>
+            </select>
+            <br/>
+            &nbsp;&nbsp;
+            <canvas id="myCanvas"></canvas>
+            <!-- three.js library -->
+            <script src = "js/three.js"></script>
+            <script src = "js/OrbitControls.js"></script>
+            <script src= "js/papercup.js"></script>
+          </span>
+
+          <span style="float:right;margin-right:85px;background-color:#FFFFFF;">
+            <!-- &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; -->
+            <br/>
+            <a class="navbar-caption text-black display-5" style="margin:0.8cm;">ORDER DETAIL</a><br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Packaging Type : ___ <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Material : ___ <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Minimum Order : 50 <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Order : ___ <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Width : <span id="demo"></span> cm <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Height : <span id="demo2"></span> cm <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Depth : <span id="demo3"></span> cm <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Quantity : ___ <br/>
+            <!-- &nbsp;&nbsp;____________________________________________________________<br/> -->
+            <br/>
+            <a class="navbar-caption text-black display-5" style="margin:0.8cm;"> INSTRUCTIONS : </a><br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1. Orbit Control : Hold Left-Click Mouse <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2. Zoom-in & Zoom-out : Scroll Mouse <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;3. The attributes will be changed<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;according the side options<br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4. Make sure all order detail already desire
+            &nbsp;&nbsp;&nbsp;&nbsp;<br/><br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <a class="btn btn-md btn-primary display-4" href="">SUBMIT</a>
+            <br/>
+            <br/>
+          </span>
+
+          <span style="clear:both;"></span>
+    </section>
+
+    <section once="" class="cid-rblxeb4aPl" id="footer6-r">
+        <div class="container">
+            <div class="media-container-row align-center mbr-white">
+                <div class="col-12">
+                    <p class="mbr-text mb-0 mbr-fonts-style display-7">
+                        Find us on :</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section once="" class="cid-rbkA1DU2DQ mbr-reveal" id="footer7-f">
+        <div class="container">
+            <div class="media-container-row align-center mbr-white">
+                <div class="row social-row">
+                    <div class="social-list align-right pb-2">
+                    <div class="soc-item">
+                            <a href="https://twitter.com/mobirise" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-instagram socicon"></span>
+                            </a>
+                        </div><div class="soc-item">
+                            <a href="https://twitter.com/mobirise" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-whatsapp socicon"></span>
+                            </a>
+                        </div><div class="soc-item">
+                            <a href="https://twitter.com/mobirise" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-facebook socicon"></span>
+                            </a>
+                        </div></div>
+                </div>
+                <div class="row row-copirayt">
+                    <p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7"></p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+      <script src="assets/web/assets/jquery/jquery.min.js"></script>
+      <script src="assets/popper/popper.min.js"></script>
+      <script src="assets/tether/tether.min.js"></script>
+      <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+      <script src="assets/smoothscroll/smooth-scroll.js"></script>
+      <script src="assets/dropdown/js/script.min.js"></script>
+      <script src="assets/parallax/jarallax.min.js"></script>
+      <script src="assets/mbr-popup-btns/mbr-popup-btns.js"></script>
+      <script src="assets/mbr-flip-card/mbr-flip-card.js"></script>
+      <script src="assets/bootstrapcarouselswipe/bootstrap-carousel-swipe.js"></script>
+      <script src="assets/mbr-testimonials-slider/mbr-testimonials-slider.js"></script>
+      <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+      <script src="assets/theme/js/script.js"></script>
+  </body>
+</html>
