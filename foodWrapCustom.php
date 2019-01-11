da65ab89428bb0691ddd2438ee807f5a9030edb8
dewisalmas <dewisalmasalsabila@gmail.com>
2019-01-11 12:54:04 +0700

    benerin footer

--- foodWrapCustom.php
+++ foodWrapCustom.php
@@ -200,41 +200,34 @@ include "menu.php";
     </section>
 
     <section once="" class="cid-rblxeb4aPl" id="footer6-r">
-        <div class="container">
-            <div class="media-container-row align-center mbr-white">
-                <div class="col-12">
-                    <p class="mbr-text mb-0 mbr-fonts-style display-7">
-                        Find us on :</p>
-                </div>
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            <div class="col-12">
+                <p class="mbr-text mb-0 mbr-fonts-style display-7">
+                    Find us on :</p>
             </div>
         </div>
-    </section>
-
-    <section once="" class="cid-rbkA1DU2DQ mbr-reveal" id="footer7-1f">
-      <div class="container">
+    </div>
+    <div class="container">
         <div class="media-container-row align-center mbr-white">
-          <div class="row social-row">
-            <div class="social-list align-right pb-2">
-              <div class="soc-item">
-                <a href="https://instagram.com/sendwish.id" target="_blank">
-                  <span class="mbr-iconfont mbr-iconfont-social socicon-instagram socicon"></span>
-                </a>
-              </div><div class="soc-item">
-                <a href="https://wa.me/6283833002258" target="_blank">
-                  <span class="mbr-iconfont mbr-iconfont-social socicon-whatsapp socicon"></span>
-                </a>
-              </div><div class="soc-item">
-                <a href="contactmail.php" target="_blank">
-                  <span class="mbr-iconfont mbr-iconfont-social socicon-facebook socicon"></span>
-                </a>
-              </div></div>
-          </div>
-          <div class="row row-copirayt">
-            <p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7"></p>
-          </div>
+            <div class="row social-row">
+                <div class="social-list align-right pb-2">
+                <div class="soc-item">
+                        <a href="https://instagram.com/sendwish.id" target="_blank">
+                            <span class="mbr-iconfont mbr-iconfont-social socicon-instagram socicon"></span>
+                        </a>
+                         <a href="https://wa.me/6283833002258" target="_blank">
+                            <span class="mbr-iconfont mbr-iconfont-social socicon-whatsapp socicon"></span>
+                        </a>
+                         <a href="contactmail.php" target="_blank">
+                            <span class="mbr-iconfont mbr-iconfont-social socicon-mail socicon"></span>
+                        </a>
+                    </div></div>
+            </div>
+            
         </div>
-      </div>
-    </section>
+    </div>
+</section>
 
 
       <script src="assets/web/assets/jquery/jquery.min.js"></script>
