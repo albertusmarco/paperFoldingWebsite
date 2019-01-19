3a7fd2b2d2a52ac0de063d661cd8218ce1952540
albertusmarco <albertusmarco@gmail.com>
2019-01-19 16:59:43 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -15,7 +15,12 @@
   $size3 = $obj['size3'];
 
   $id_product = mysqli_fetch_row(viewData("select id_product from product where name='$type'"));
-  print_r($id_product);
+  print_r($id_product[0]);
+  $id_customer = $_SESSION['idcust'];
+  print_r($id_customer);
+  $tmp = mysqli_fetch_row(viewData("SELECT COUNT(*) FROM `order`;"));
+  $id_order = (int) $tmp[0] + 1 ;
+  print_r($id_order);
   // $sql = "INSERT INTO customer (NAME,email,phone,address,postal_code) VALUES('$judul', 'coba@gmail.com', '08123456789', 'Surabaya', '65151');";
   // dml($sql);
 ?>
