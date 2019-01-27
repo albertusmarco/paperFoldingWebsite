69342a1a62e45c2463233265cda25a1b73a0e1ed
faniaacecillia <45564782+faniaacecillia@users.noreply.github.com>
2019-01-27 14:04:03 +0700

    spya shop bisa kebuka walaupun belum login

--- menu.php
+++ menu.php
@@ -51,11 +51,10 @@ session_start();
                         Portfolio</a></li><li class="nav-item"><a class="nav-link link text-black display-4" href="index.php#testimonials-slider1-b">
                         About</a></li><li class="nav-item"><a class="nav-link link text-black display-4" href="
                           <?php
-                          if($_SESSION['adasesi']==1){
-                            echo('shop.php');
-                          }else{
-                            echo('noshop.php');
-                          }
+						  if(!isset($_SESSION['adasesi']))
+						  {echo('noshop.php');}
+						  elseif($_SESSION['adasesi']==1){
+                            echo('shop.php');}
                           ?>">
                         Shop</a></li>
                 <li class="nav-item">
