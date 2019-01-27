18d799dbee2d52c01befe45e4b0d9c2c5e0438b2
albertusmarco <albertusmarco@gmail.com>
2019-01-27 14:16:01 +0700

    Update index.php
    Tambahan dari fania

--- index.php
+++ index.php
@@ -4,20 +4,20 @@ include "menu.php";
 <!DOCTYPE html>
 <html>
 <head>
-  
-  
-  
-  
+
+
+
+
 </head>
 <body>
 
 
 <section class="engine"><a href="https://mobirise.info/u">bootstrap html templates</a></section><section class="header10 cid-rbkq4YRq2S mbr-fullscreen mbr-parallax-background" id="header10-3">
-    
+
 
     <div class="container">
         <div class="media-container-column mbr-white col-lg-8 col-md-10 ml-auto">
-            
+
             <h3 class="mbr-section-subtitle align-right mbr-light pb-3 mbr-fonts-style display-2"><em>Easier Way to Make
 </em><div><em>Packaging!</em></div></h3>
             <p class="mbr-text align-right pb-3 mbr-fonts-style display-5">
@@ -25,168 +25,169 @@ include "menu.php";
             </p>
             <div class="mbr-section-btn align-right"><a class="btn btn-md btn-primary-outline display-4" href="index.php#features18-k">TUTORIAL</a>
                 <a class="btn btn-md btn-primary display-4" href="
-        <?php
-                          if($_SESSION['adasesi']==1){
-                            echo('shop.php');
-                          }else{
-                            echo('noshop.php');
-                          }
-                          ?>
+                <?php
+                  if(!isset($_SESSION['adasesi'])){
+                    echo('noshop.php');
+                  }
+                  elseif($_SESSION['adasesi']==1){
+                    echo('shop.php');
+                  }
+                ?>
                 ">GO TO SHOP</a></div>
         </div>
     </div>
 
-    
+
 </section>
 
 <section class="features18 popup-btn-cards cid-rbljldAFZH" id="features18-k">
 
-    
 
-    
+
+
     <div class="container">
         <h2 class="mbr-section-title pb-3 align-center mbr-fonts-style display-2">
             Step by Step Tutorial</h2>
         <h3 class="mbr-section-subtitle display-5 align-center mbr-fonts-style mbr-light">
             1. Choose your packaging type</h3>
-        
-   
 
-    
+
+
+
         <div class="media-container-row">
             <div class="card p-3 col-12 col-md-6 col-lg-3">
-             
+
                     <div class="card-img">
                         <img src="assets/images/cardboard-fix-492x492.png" alt="Mobirise" title="">
                     </div>
                     <div class="card-box">
                         <h4 class="card-title mbr-fonts-style display-7">
                             Cardboard Box</h4>
-                        
+
                     </div>
                     <div class="mbr-section-btn text-center"><a href="cardboardbox.php" class="btn btn-primary display-4">
                             Learn More
                         </a></div>
-                
+
             </div>
 
             <div class="card p-3 col-12 col-md-6 col-lg-3">
-              
+
                     <div class="card-img">
                         <img src="assets/images/paper-cup-fix-2-492x492.png" alt="Mobirise" title="">
                     </div>
                     <div class="card-box">
                         <h4 class="card-title mbr-fonts-style display-7">
                             Paper Cup</h4>
-                        
+
                     </div>
                     <div class="mbr-section-btn text-center"><a href="papercupp.php" class="btn btn-primary display-4">
                             Learn More
                         </a></div>
-               
+
             </div>
 
             <div class="card p-3 col-12 col-md-6 col-lg-3">
-               
+
                     <div class="card-img">
                         <img src="assets/images/paper-pouch-fix-492x492.png" alt="Mobirise" title="">
                     </div>
                     <div class="card-box">
                         <h4 class="card-title mbr-fonts-style display-7">
                             Paper Pouch</h4>
-                        
+
                     </div>
                     <div class="mbr-section-btn text-center"><a href="paperpouch.php" class="btn btn-primary display-4">
                             Learn More
                         </a></div>
-                
+
             </div>
 
             <div class="card p-3 col-12 col-md-6 col-lg-3">
-              
+
                     <div class="card-img">
                         <img src="assets/images/food-wrap-fix-492x492.png" alt="Mobirise" title="">
                     </div>
                     <div class="card-box">
                         <h4 class="card-title mbr-fonts-style display-7">
                             Food Wrap</h4>
-                        
+
                     </div>
                     <div class="mbr-section-btn text-center"><a href="foodwrap.php" class="btn btn-primary display-4">
                             Learn More
                         </a></div>
-            
+
             </div>
         </div>
-      
+
         <div class="media-container-row">
             <div class="card p-3 col-12 col-md-6 col-lg-3">
-              
+
                     <div class="card-img">
                         <img src="assets/images/paper-bag-fix-492x492.png" alt="Mobirise" title="">
                     </div>
                     <div class="card-box">
                         <h4 class="card-title mbr-fonts-style display-7">
                             Paper Bag</h4>
-                        
+
                     </div>
                     <div class="mbr-section-btn text-center"><a href="paperbag.php" class="btn btn-primary display-4">
                             Learn More</a></div>
-               
+
             </div>
 
             <div class="card p-3 col-12 col-md-6 col-lg-3">
-               
+
                     <div class="card-img">
                         <img src="assets/images/pack-label-492x492.png" alt="Mobirise" title="">
                     </div>
                     <div class="card-box">
                         <h4 class="card-title mbr-fonts-style display-7">
                             Pack Label</h4>
-                        
+
                     </div>
                     <div class="mbr-section-btn text-center"><a href="packlabel.php" class="btn btn-primary display-4">
                             Learn More</a></div>
-                
+
             </div>
 
             <div class="card p-3 col-12 col-md-6 col-lg-3">
-              
+
                     <div class="card-img">
                         <img src="assets/images/soft-box-super-fix-492x492.png" alt="Mobirise" title="">
                     </div>
                     <div class="card-box">
                         <h4 class="card-title mbr-fonts-style display-7">
                             Soft Box</h4>
-                        
+
                     </div>
                     <div class="mbr-section-btn text-center"><a href="softbox.php" class="btn btn-primary display-4">
                             Learn More</a></div>
-              
+
             </div>
 
             <div class="card p-3 col-12 col-md-6 col-lg-3">
-            
+
                     <div class="card-img">
                         <img src="assets/images/plastic-pouch-fix-492x492.png" alt="Mobirise" title="">
                     </div>
                     <div class="card-box">
                         <h4 class="card-title mbr-fonts-style display-7">
                             Plastic Pouch</h4>
-                        
+
                     </div>
                     <div align="center" class="mbr-section-btn text-center"><a href="plasticpouch.php" class="btn btn-primary display-4">
                             Learn More</a></div>
-                
+
             </div>
-      
+
     </div>
      </div>
 </section>
 
 <section class="mbr-section article content1 cid-rblls8eMxl" id="content1-m">
-    
-     
+
+
 
     <div class="container">
         <div class="media-container-row">
@@ -197,7 +198,7 @@ include "menu.php";
 
 <section class="features3 cid-rbltC75YZE" id="features3-q">
 
-    
+
     <div class="container">
         <div class="media-container-row">
             <div class="card p-3 col-12 col-md-6 col-lg-4">
@@ -212,7 +213,7 @@ include "menu.php";
                             Available in 150 gr, 210 gr, 230 gr, 260 gr, 310 gr
                         </p>
                     </div>
-                    
+
                 </div>
             </div>
 
@@ -228,7 +229,7 @@ include "menu.php";
                             Available in 210 gr, 230 gr, 250 gr, 310 gr
                         </p>
                     </div>
-                    
+
                 </div>
             </div>
 
@@ -243,11 +244,11 @@ include "menu.php";
                         <p class="mbr-text mbr-fonts-style display-7">
                             Available in&nbsp;70 gr, 80 gr, 150 gr </p>
                     </div>
-                    
+
                 </div>
             </div>
 
-            
+
         </div>
     </div>
 </section>
@@ -267,7 +268,7 @@ include "menu.php";
                         <p class="mbr-text mbr-fonts-style display-7">
                             Available in 230 gr, 250 gr, 270 gr, 300 gr </p>
                     </div>
-                    
+
                 </div>
             </div>
 
@@ -282,7 +283,7 @@ include "menu.php";
                         <p class="mbr-text mbr-fonts-style display-7">
                             Available in 150 gr </p>
                     </div>
-                    
+
                 </div>
             </div>
 
@@ -296,18 +297,18 @@ include "menu.php";
                             Corrugated</h4>
                         <p class="mbr-text mbr-fonts-style display-7"></p>
                     </div>
-                    
+
                 </div>
             </div>
 
-            
+
         </div>
     </div>
 </section>
 
 <section class="mbr-section article content1 cid-rblmrLHJ4T" id="content1-o">
-    
-     
+
+
 
     <div class="container">
         <div class="media-container-row">
@@ -318,20 +319,21 @@ include "menu.php";
 
 <section class="mbr-section content8 cid-rblmBcSF3C" id="content8-p">
 
-    
+
 
     <div class="container">
         <div class="media-container-row title">
             <div class="col-12 col-md-8">
                 <div class="mbr-section-btn align-center"><a class="btn btn-primary display-4" href="
-             <?php
-                          if($_SESSION['adasesi']==1){
-                            echo('shop.php');
-                          }else{
-                            echo('noshop.php');
-                          }
-                          ?>">
-                    OK, I'm Ready!</a></div>
+                  <?php
+                    if(!isset($_SESSION['adasesi'])){
+                      echo('noshop.php');
+                    }
+                    elseif($_SESSION['adasesi']==1){
+                      echo('shop.php');
+                    }
+                  ?>
+              ">OK, I'm Ready!</a></div>
             </div>
         </div>
     </div>
@@ -339,12 +341,12 @@ include "menu.php";
 
 <section class="features15 cid-rbksZer75u" id="features15-9">
 
-    
 
-    
+
+
     <div class="container">
         <h2 class="mbr-section-title pb-3 align-center mbr-fonts-style display-2">Our Recent Projects</h2>
-        
+
 
         <div class="media-container-row container pt-5 mt-2">
 
@@ -365,7 +367,7 @@ include "menu.php";
             </div>
 
             <div class="col-12 col-md-6 mb-4 col-lg-3">
-                
+
                 <div class="card flip-card p-5 align-center">
                     <div class="card-front card_cont">
                         <img src="assets/images/template-porto-03-4.18.33-pm-526x366.jpg" alt="Mobirise" title="">
@@ -377,7 +379,7 @@ include "menu.php";
                     </div>
                 </div>
             </div>
-            
+
             <div class="col-12 col-md-6 mb-4 col-lg-3">
                 <div class="card flip-card p-5 align-center">
                     <div class="card-front card_cont">
@@ -389,7 +391,7 @@ include "menu.php";
                             <br>Coffee Lab Packaging &amp; Brand Design
                         </p>
                     </div>
-                </div> 
+                </div>
             </div>
 
             <div class="col-12 col-md-6 mb-4 col-lg-3">
@@ -403,24 +405,24 @@ include "menu.php";
                             <br>Kantong Tea Packaging &amp; Brand Design
                         </p>
                     </div>
-                </div> 
+                </div>
             </div>
         </div>
 </div></section>
 
 <section class="carousel slide testimonials-slider cid-rbkx64htco" id="testimonials-slider1-b">
-    
-    
 
-    
+
+
+
 
     <div class="container text-center">
         <h2 class="pb-5 mbr-fonts-style display-2">MEET OUR TEAM</h2>
 
         <div class="carousel slide" data-ride="carousel" role="listbox">
             <div class="carousel-inner">
-                
-                
+
+
             <div class="carousel-item">
                     <div class="user col-md-8">
                         <div class="user_image">
@@ -452,7 +454,7 @@ include "menu.php";
                   <span aria-hidden="true" class="mbri-arrow-prev mbr-iconfont"></span>
                   <span class="sr-only">Previous</span>
                 </a>
-                
+
                 <a class="carousel-control-next" role="button" data-slide="next">
                   <span aria-hidden="true" class="mbri-arrow-next mbr-iconfont"></span>
                   <span class="sr-only">Next</span>
@@ -488,7 +490,7 @@ include "menu.php";
                         </a>
                     </div></div>
             </div>
-            
+
         </div>
     </div>
 </section>
@@ -508,9 +510,9 @@ include "menu.php";
   <script src="assets/mbr-testimonials-slider/mbr-testimonials-slider.js"></script>
   <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
   <script src="assets/theme/js/script.js"></script>
-  
-  
-  
-  
+
+
+
+
 </body>
 </html>
