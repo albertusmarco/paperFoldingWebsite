2cf150ea59f5dcbca62b120599a18fca8789e3fa
faniaacecillia <45564782+faniaacecillia@users.noreply.github.com>
2019-02-01 12:20:25 +0700

    update database (id invoice dihilangin)

--- database/pfw.sql
+++ database/pfw.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 19, 2019 at 09:20 AM
+-- Generation Time: Feb 01, 2019 at 06:17 AM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -43,7 +43,10 @@ INSERT INTO `customer` (`id_customer`, `name`, `email`, `phone`, `address`, `pos
 ('C1', 'Fania Cecillia', 'fcecillia@student.ciputra.ac.id', '087856933365', 'Wiyung', 60228),
 ('C2', 'Albertus Marco', 'amarco@student.ciputra.ac.id', '088966548595', 'Pakuwon', 60112),
 ('C3', 'Dewi Salma Salsabila', 'dsalma@student.ciputra.ac.id', '081265996325', 'Pakuwon', 60112),
-('C4', 'Dhienalight', 'dhienalight@student.ciputra.ac.id', '081256692145', 'Sepanjang', 61257);
+('C4', 'Dhienalight', 'dhienalight@student.ciputra.ac.id', '081256692145', 'Sepanjang', 61257),
+('C5', 'Natalie Parametha', 'natalieparametha@gmail.com', '083833002258', 'Jalan Gatot Subroto 91  Probolinggo', 621217),
+('C6', 'huha', '88', '88', '88', 88),
+('C7', 'Priskila Fania', 'pf@gmail.com', '9383', 'haha', 8383);
 
 -- --------------------------------------------------------
 
@@ -64,7 +67,9 @@ INSERT INTO `design_detail` (`id_order`, `path_detail`) VALUES
 ('O1', 'detail/detail1.png'),
 ('O3', 'detail/detail3.png'),
 ('O5', 'detail/detail5.png'),
-('O5', 'detail/detail55.png');
+('O5', 'detail/detail55.png'),
+('O6', 'asset/6944985-deer-art.jpg'),
+('O9', 'asset/7024877-siberian-samoyed.jpg');
 
 -- --------------------------------------------------------
 
@@ -83,13 +88,16 @@ CREATE TABLE IF NOT EXISTS `login` (
 --
 
 INSERT INTO `login` (`email`, `password`, `role`) VALUES
+('88', '88', 2),
 ('admin', 'admin', 1),
 ('amarco@student.ciputra.ac.id', 'ampass', 2),
 ('bobby@gmail.com', 'bpass', 2),
 ('dhienalight@student.ciputra.ac.id', 'dpass', 2),
 ('dsalma@student.ciputra.ac.id', 'dsspass', 2),
 ('fcecillia@student.ciputra.ac.id', 'fcpass', 2),
-('jj', 'pass', 2);
+('jj', 'pass', 2),
+('natalieparametha@gmail.com', 'changnailie', 2),
+('pf@gmail.com', '1234', 2);
 
 -- --------------------------------------------------------
 
@@ -109,7 +117,6 @@ CREATE TABLE IF NOT EXISTS `order` (
   `size2` varchar(45) DEFAULT NULL,
   `size3` varchar(45) DEFAULT NULL,
   `path_custom` varchar(50) NOT NULL,
-  `id_invoice` varchar(50) NOT NULL,
   `qty` int(10) unsigned NOT NULL,
   `price` int(10) unsigned NOT NULL,
   `status` varchar(50) NOT NULL,
@@ -122,12 +129,20 @@ CREATE TABLE IF NOT EXISTS `order` (
 -- Dumping data for table `order`
 --
 
-INSERT INTO `order` (`id_order`, `id_customer`, `id_product`, `material`, `combination`, `lamination`, `available`, `size1`, `size2`, `size3`, `path_custom`, `id_invoice`, `qty`, `price`, `status`, `order_time`, `path_dp`, `path_paid`) VALUES
-('O1', 'C2', 'P1', 'Corrugated', 'Sticker Vinly', NULL, NULL, NULL, NULL, NULL, 'custom/custom1.obj', 'IV160119-1', 20, 100000, 'Complete', '2019-01-16 16:01:53', 'dp/dp1.jpg', 'paid/paid1.jpg'),
-('O2', 'C2', 'P3', 'Polylethylene [PE] Water Proof', 'Sticker Vinly', NULL, NULL, NULL, NULL, NULL, 'custom/custom2.obj', 'IV160119-3', 30, 200000, 'Complete', '2019-01-16 16:13:16', 'dp/dp2.jpg', 'paid/paid2.jpg'),
-('O3', 'C3', 'P2', 'Art Paper 210 gr', 'Hot Print', 'Doff', NULL, NULL, NULL, NULL, 'custom/custom3.obj', 'IV160119-2', 35, 250000, 'In Progress', '2019-01-16 16:08:07', 'dp/dp3.jpg', 'paid/paid3.jpg'),
-('O4', 'C1', 'P2', 'Ivory 230 gr', 'UV Print', 'Glossy', NULL, NULL, NULL, NULL, 'custom/custom4.obj', 'IV180119-1', 10, 100000, 'Cancel', '2019-01-16 16:08:01', 'dp/dp4.jpg', 'paid/paid4.jpg'),
-('O5', 'C4', 'P2', 'Samson 70 gr', 'UV Print', 'Glossy', NULL, NULL, NULL, NULL, 'custom/custom5.obj', 'IV200119-1', 10, 100000, '-', '2019-01-19 06:05:20', 'dp/dp5.jpg', 'paid/paid5.jpg');
+INSERT INTO `order` (`id_order`, `id_customer`, `id_product`, `material`, `combination`, `lamination`, `available`, `size1`, `size2`, `size3`, `path_custom`, `qty`, `price`, `status`, `order_time`, `path_dp`, `path_paid`) VALUES
+('O1', 'C2', 'P1', 'Corrugated', 'Sticker Vinly', NULL, NULL, NULL, NULL, NULL, 'custom/custom1.obj', 20, 100000, 'In Progress', '2019-01-16 16:01:53', 'dp/dp1.jpg', 'paid/paid1.jpg'),
+('O10', 'C1', 'P3', 'Polylethylene Water Proof', 'Sablon', 'none', 'none', '6', '5', '5', 'custom/Paper Cup4.json', 100, 1000, '-', '2019-01-21 03:40:31', '', ''),
+('O11', 'C5', '', 'Corrugated', 'UV Print', 'none', 'none', '60', '40', '20', 'custom/Card Board1.json', 50, 50000, '-', '2019-01-21 04:53:20', '', ''),
+('O12', 'C1', 'P2', 'Art Paper 150 gr', 'Sticker Vinyl', 'none', 'none', '50', '60', '15', 'custom/Paper Bag5.json', 50, 7000, '-', '2019-01-21 04:59:31', '', ''),
+('O13', 'C7', 'P7', 'Art Paper 150 gr', 'Sticker Vinyl', 'Doff', 'none', '50', '50', '50', 'custom/Soft Box1.json', 50, 30000, '-', '2019-01-21 17:28:04', '', ''),
+('O2', 'C2', 'P3', 'Polylethylene [PE] Water Proof', 'Sticker Vinly', NULL, NULL, NULL, NULL, NULL, 'custom/custom2.obj', 30, 200000, 'Complete', '2019-01-16 16:13:16', 'dp/dp2.jpg', 'paid/paid2.jpg'),
+('O3', 'C3', 'P2', 'Art Paper 210 gr', 'Hot Print', 'Doff', NULL, NULL, NULL, NULL, 'custom/custom3.obj', 35, 250000, 'In Progress', '2019-01-16 16:08:07', 'dp/dp3.jpg', 'paid/paid3.jpg'),
+('O4', 'C1', 'P2', 'Ivory 230 gr', 'UV Print', 'Glossy', NULL, NULL, NULL, NULL, 'custom/custom4.obj', 10, 100000, 'Cancel', '2019-01-16 16:08:01', 'dp/dp4.jpg', 'paid/paid4.jpg'),
+('O5', 'C4', 'P2', 'Samson 70 gr', 'UV Print', 'Glossy', NULL, NULL, NULL, NULL, 'custom/custom5.obj', 10, 100000, '-', '2019-01-19 06:05:20', 'dp/dp5.jpg', 'paid/paid5.jpg'),
+('O6', 'C1', 'P2', 'none', 'Hot Print', 'none', 'none', '50', '60', '15', 'custom/Paper Bag2.json', 50, 12000, 'In Progress', '2019-01-20 13:14:40', '', ''),
+('O7', 'C1', 'P8', 'Food Grade Paper', 'Sticker Vinyl', 'none', 'none', '60', '50', '10', 'custom/Food Wrap3.json', 50, 800, '-', '2019-01-20 13:35:44', '', ''),
+('O8', '', 'P3', 'Polylethylene Water Proof', 'Sablon', 'none', 'none', '6', '5', '5', 'custom/Paper Cup1.json', 50, 1500, '-', '2019-01-21 03:21:21', '', ''),
+('O9', '', '', 'Corrugated', 'UV Print', 'none', 'none', '60', '11', '20', 'custom/Card Board2.json', 67, 35000, '-', '2019-01-21 03:37:22', '', '');
 
 -- --------------------------------------------------------
 
